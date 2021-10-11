e1c1fa4b27ff55ae85744944a56076007b5d7bd0
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-11 16:09:55 -0700

    created function to render cards, imported chicken list and render to app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // import functions and grab DOM elements
-
+import { renderChicken } from './render.js';
+import { chickens } from './chicken.js';
 // initialize global state
+const chickenList = document.getElementById('chicken-list');
+
+for (let chicken of chickens){
+  const chickenCard = renderChicken(chicken);
+  chickenList.append(chickenCard);
+}
+
 
 // set event listeners 
   // get user input
--- chicken.js
+++ chicken.js
@@ -4,33 +4,38 @@ export const chickens = [
         breed: 'Leghorn',
         color: 'white',
         eggPerWeekAvg: '4',
-        demeanor: 'exciteable'},
+        demeanor: 'exciteable',
+        img: './assets/whitechicken.png'},
     {
         id: '2',
-        breed: 'Rhose Island Red',
+        breed: 'RI Red',
         color: 'red',
         eggPerWeekAvg: '3',
-        demeanor: 'laid-back'},
+        demeanor: 'laid-back',
+        img: './assets/redchicken.png'},
     {
         id: '3',
         breed: 'Americana',
         color: 'brown',
         eggPerWeekAvg: '2',
-        demeanor: 'laid-back'
+        demeanor: 'laid-back',
+        img: './assets/yellowblackandbrownchicken.png'
     },
     {
         id: '4',
         breed: 'Orpington',
         color: 'yellow',
         eggPerWeekAvg: '3',
-        demeanor: 'exciteable'
+        demeanor: 'exciteable',
+        img: './assets/yellowchicken.png'
     },
     {
         id: '5',
         breed: 'Easter-Egger',
         color: 'grey',
         eggPerWeekAvg: '4',
-        demeanor: 'exciteable'
+        demeanor: 'exciteable',
+        img: "./assets/brownchicken.png"
     }
 
     
