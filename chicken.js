1fd12c83582c42e296d1deb8c1b8d377add0c4e7
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2021-10-11 15:15:50 -0700

    created chicken list and exported it

---
+++ chicken.js
@@ -0,0 +1,38 @@
+export const chickens = [
+    {
+        id: '1',
+        breed: 'Leghorn',
+        color: 'white',
+        eggPerWeekAvg: '4',
+        demeanor: 'exciteable'},
+    {
+        id: '2',
+        breed: 'Rhose Island Red',
+        color: 'red',
+        eggPerWeekAvg: '3',
+        demeanor: 'laid-back'},
+    {
+        id: '3',
+        breed: 'Americana',
+        color: 'brown',
+        eggPerWeekAvg: '2',
+        demeanor: 'laid-back'
+    },
+    {
+        id: '4',
+        breed: 'Orpington',
+        color: 'yellow',
+        eggPerWeekAvg: '3',
+        demeanor: 'exciteable'
+    },
+    {
+        id: '5',
+        breed: 'Easter-Egger',
+        color: 'grey',
+        eggPerWeekAvg: '4',
+        demeanor: 'exciteable'
+    }
+
+    
+
+]
